e7aa57ab992b3487fd8a9c810734f6b6c2a446dd
Dark-Revel431 <67160704+Dark-Revel431@users.noreply.github.com>
2021-09-02 18:39:21 +0200

    Add files via upload

---
+++ src/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ReqMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reader read = new Reader();
+            Verify ver = new Verify();
+
+            string FileToRead = ver.VerFileToRead(args);
+            if (FileToRead == null)
+            {
+                Console.WriteLine("No [FileToRead] param!");
+                Environment.Exit(1);
+            }
+
+            bool FileExist = ver.VerExistFile(FileToRead);
+            if (FileExist == false)
+            {
+                Console.WriteLine("The file [" + FileToRead + "] doesn't exist!");
+                Environment.Exit(1);
+            }
+
+            bool CanOpenFile = ver.VerReadFile(FileToRead);
+            if (CanOpenFile == false)
+            {
+                Console.WriteLine("Cannot read the file [" + FileToRead + "]!");
+                Environment.Exit(1);
+            }
+
+            read.ReadFile(FileToRead);
+        }
+    }
+}
---
+++ src/Reader.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReqMaker
+{
+    class Reader
+    {
+        public void ReadFile(string FileToRead)
+        {
+            List<string> pip = new List<string>();
+            string line;
+            System.IO.StreamReader file = new System.IO.StreamReader(FileToRead);
+            while ((line = file.ReadLine()) != null)
+            {
+                String[] lineS = line.Split(',', ' ', '\t');
+                for (int x = 0; x < lineS.Length; x++)
+                {
+                    if (lineS[x].Equals("import"))
+                    {
+                        pip.Add(lineS[x + 1]);
+                        for (int y = x + 2; y < lineS.Length; y++)
+                        {
+                            if (lineS[y].Equals(""))
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                pip.Add(lineS[y]);
+                            }
+                        }
+                    }
+                    else if (lineS[x].Equals("from"))
+                    {
+                        pip.Add(lineS[x + 1]);
+                        x += 2;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            file.Close();
+
+            List<string> newPip = new List<string>();
+            foreach (var pi in pip)
+            {
+                if (pi.Contains('.'))
+                {
+                    String[] piS = pi.Split('.');
+                    newPip.Add(piS[0]);
+                }
+                else
+                {
+                    newPip.Add(pi);
+                }
+            }
+
+            Writer write = new Writer();
+            write.WriteFile(newPip);
+        }
+    }
+}
